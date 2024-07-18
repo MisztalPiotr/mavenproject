d0954994fdb5a64dc4af2b33fbe7afcc56afd9dc
unknown <misztalpiotr99@gmail.com>
2024-07-18 19:19:46 +0100

    test

---
+++ src/main/java/printOs/com/Car.java
@@ -0,0 +1,16 @@
+package printOs.com;
+
+public class Car {
+	int model;
+	String carName;
+	public void display(){
+	System.out.println("Model:"+model);
+	System.out.println("Name:"+carName);
+	System.out.println("Model:"+model);
+	System.out.println("Name:"+carName);
+	System.out.println("Model:"+model);
+	System.out.println("Name:"+carName);
+	System.out.println("Model:"+model);
+	System.out.println("Name:"+carName);
+	}
+}
